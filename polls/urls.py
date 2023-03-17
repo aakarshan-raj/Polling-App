5055d012d5cca93a6856004fced7b5c3e70bf590
aakarshan-raj <aakarshan7raj@gmail.com>
2023-03-17 16:52:37 +0530

    almost part 3 done

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index),
+    path('<int:question_id>/',views.details),
+    path('<int:question_id>/result/',views.result),
+    path('<int:question_id>/vote/',views.vote)
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>hello</h1>")
+    try:
+       obj = Question.objects.order_by('-pub_date')
+    except:
+        raise Http404("Model doesn't exists")   
+    output = ','.join(q.question_text for q in obj)
+    context = {"data":obj}
+    return render(request,'polls/index.html',context)
+
+def details(request,question_id):
+    obj = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/details.html",context={"question":obj})
+
+def result(request,question_id):
+    response = "result of:"+str(question_id)
+    return HttpResponse(response)
+
+def vote(request,question_id):
+    response = "vote of:"+str(question_id)
+    return HttpResponse(response)
